c737f0eb739e4648f3d66fc83c4e22da115ad716
Dennis Sulejman <45164504+dennissulejman@users.noreply.github.com>
2023-12-04 21:41:52 +0100

    Delete unneeded comment

--- Day 2/rust_two/src/main.rs
+++ Day 2/rust_two/src/main.rs
@@ -4,8 +4,6 @@ use std::{
     path::Path,
 };
 
-// The Elf would first like to know which games would have been possible if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
-
 const INPUT_FILE: &str = "../input.txt";
 
 const GAME: &str = "Game";
